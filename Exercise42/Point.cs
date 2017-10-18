bb1015edf13e9ef61c79884b5bb981bbbe2f89c5
dowskee <31831031+dowskee@users.noreply.github.com>
2017-10-18 15:33:03 -0400

    First commit

---
+++ Exercise42/Point.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise42
+{
+    class Point
+    {
+        private int x;
+        private int y;
+
+        public int VariableX
+        {
+            set { x = value; }
+            get { return x; }
+        }
+
+        public int VariableY
+        {
+            set { y = value; }
+            get { return y; }
+        }
+
+        public Point()
+        {
+            VariableX = 0;
+            VariableY = 0;
+        }
+
+        public Point(int ex, int why)
+        {
+            VariableX = ex;
+            VariableY = why;
+        }
+    }
+}
---
+++ Exercise42/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Point> Entry = new List<Point>();
+
+            Console.WriteLine("Please enter an X coordinate:");
+            int ex = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter a Y coordinate:");
+            int why = int.Parse(Console.ReadLine());
+
+            Point temp = new Point(ex, why);
+
+            Entry.Add(temp);
+
+            Console.WriteLine($"You have created a point object ({ex}, {why})");
+        }
+    }
+}
