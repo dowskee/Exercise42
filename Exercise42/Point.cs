0896048fdb6bfa4564f1d07086b11d3e456b2432
dowskee <31831031+dowskee@users.noreply.github.com>
2017-10-18 16:30:15 -0400

    Exercise 44

--- Exercise42/Point.cs
+++ Exercise42/Point.cs
@@ -10,6 +10,8 @@ namespace Exercise42
     {
         private double x;
         private double y;
+        private double q;
+        private double z;
 
         public double VariableX
         {
@@ -23,23 +25,41 @@ namespace Exercise42
             get { return y; }
         }
 
+        public double VariableQ
+        {
+            set { q = value; }
+            get { return q; }
+        }
+
+        public double VariableZ
+        {
+            set { z = value; }
+            get { return z; }
+        }
+
         public Point()
         {
             VariableX = 0;
             VariableY = 0;
+            VariableQ = 0;
+            VariableZ = 0;
         }
 
-        public Point(double ex, double why)
+        public Point(double ex, double why, double queue, double zee)
         {
             VariableX = ex;
             VariableY = why;
+            VariableQ = queue;
+            VariableZ = zee;
         }
 
         public double CalculateDistance()
         {
-            double dist = Math.Sqrt((VariableX * VariableX) + (VariableY * VariableY));
-            Console.WriteLine($"It has a distance of {dist}");
-            return dist;
+            double mid1 = ((VariableX + VariableQ)/2);
+            double mid2 = ((VariableY + VariableZ) / 2);
+            Console.WriteLine($"The midpoint is ({mid1}, {mid2})");
+            return mid1;
+            return mid2;
           
         }
 
--- Exercise42/Program.cs
+++ Exercise42/Program.cs
@@ -19,7 +19,13 @@ namespace Exercise42
             Console.WriteLine("Please enter a Y coordinate:");
             int why = int.Parse(Console.ReadLine());
 
-            Point temp = new Point(ex, why);
+            Console.WriteLine("Please enter a Q coordinate:");
+            int queue = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter a Z coordinate:");
+            int zee = int.Parse(Console.ReadLine());
+
+            Point temp = new Point(ex, why, queue, zee);
 
             Entry.Add(temp);
 
